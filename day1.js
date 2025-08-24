5b4043baf867c572f75942b057c8b633e5335e44
Komal-2108 <thekomal2502@gmail.com>
2025-08-24 23:54:26 +0530

    Day1 task : Print student details from an object.

---
+++ day1.js
@@ -0,0 +1,16 @@
+// Define a student object
+const student = {
+  name: "Komal Sharma",
+  age: 18,
+  course: "B.Tech CSE",
+  rollNo: "CSE101",
+  college: "LNCT Bhopal"
+};
+
+// Print student details
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("Roll Number:", student.rollNo);
+console.log("College:", student.college);
